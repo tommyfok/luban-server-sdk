b6f74415ccf0dd0b574a0115f39504e3a0682e25
tommyfok <154274174@qq.com>
2019-10-10 21:29:33 +0800

    finish instance.cache/instance.utils

--- index.js
+++ index.js
@@ -51,6 +51,54 @@ class Luban {
     })
     this.db.sys = KnexInstances[this.id]
     this.utils = require('./modules/utils')(this)
+    let redisInst = RedisInstances[this.id]
+    this.redis = redisInst
+    this.cache = {
+      get(key) {
+        return new Promise((resolve, reject) => {
+          redisInst.get(key, (err, result) => {
+            if (err) {
+              console.log('asyncGet failed', key, err)
+              reject(err)
+            } else {
+              let _result = result
+              if (typeof result === 'string') {
+                try {
+                  _result = JSON.parse(result)
+                } catch (e) {}
+              }
+              resolve(_result)
+            }
+          })
+        })
+      },
+      set(key, value) {
+        let _value = typeof value === 'string' ? value : JSON.stringify(value)
+        return new Promise((resolve, reject) => {
+          redisInst.set(key, _value, (err, result) => {
+            if (err) {
+              console.log('set cache failed', key, value, err)
+              reject(err)
+            } else {
+              resolve(result)
+            }
+          })
+        })
+      },
+      setEx(key, value, timeout = 7190) {
+        let _value = typeof value === 'string' ? value : JSON.stringify(value)
+        return new Promise((resolve, reject) => {
+          redisInst.set(key, _value, 'EX', timeout, (err, result) => {
+            if (err) {
+              console.log('setEx failed', key, value, err)
+              reject(err)
+            } else {
+              resolve(result)
+            }
+          })
+        })
+      }
+    }
   }
 
   async _getApp(appid) {
